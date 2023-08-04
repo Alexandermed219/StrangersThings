0e2c7d2f794b1d3297e7571243deabd079807b00
Alexander Medina <alexandermed219@gmail.com>
2023-08-04 15:58:50 -0400

    Nav bar testing

--- Strangers/src/App.jsx
+++ Strangers/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
 
+const Home = () => <div>Home Page</div>;
+const About = () => <div>About Page</div>;
+const Contact = () => <div>Contact Page</div>;
+
+const App = () => {
   return (
-   <div>
-    
-   </div>
-  )
-}
+    <Router>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/contact" component={Contact} />
+    </Router>
+  );
+};
 
-export default App
+export default App;
